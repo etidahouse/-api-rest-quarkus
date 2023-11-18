c7d57a4251570384053101299baae18d8a5641dc
Étienne <e-begon@hotmail.fr>
2023-11-18 09:56:25 +0100

    wip: system is changing constantly

---
+++ src/main/java/com/example/restapi/HelloController.java
@@ -0,0 +1,14 @@
+package main.java.com.example.restapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+}
